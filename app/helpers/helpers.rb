5fa222c45cb297f38057df1466c1521e19c3e03e
Chris Cardle <cdcardle@gmail.com>
2018-04-02 16:57:11 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,9 @@
 class Helpers
-  def current_user(session_hash)
+  def self.current_user(session_hash)
     User.find(session_hash[:user_id])
   end
 
-  def is_logged_in?(session_hash)
+  def self.is_logged_in?(session_hash)
     id == session_hash[:user_id] ? true : false
   end
 end
